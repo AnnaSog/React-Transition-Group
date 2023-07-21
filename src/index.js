ec1317fcff408046c0e365af4edfc752c894ea24
Vartanush <anna87-07@inbox.ru>
2023-07-21 17:01:40 +0300

    Transition

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const obj = {
+//     name: 'Anna',
+//     old: 20,
+// }
+  
+// const elem = (
+//   <div>
+//     <h1>{obj.old}</h1>
+//   </div>
+  
+// )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App/>
+
+);
+
+
